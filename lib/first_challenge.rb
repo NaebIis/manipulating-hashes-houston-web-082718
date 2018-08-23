7a2d9fd7c5182be82d40ed8e28dfaf7158747deb
John Grubbs <johnmlowery@gmail.com>
2018-08-23 23:25:02 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,6 +18,9 @@ contacts.each do |person, data|
   data.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
       value.pop
+    end
+  end
+end
 
   #remember to return your newly altered contacts hash!
   contacts
